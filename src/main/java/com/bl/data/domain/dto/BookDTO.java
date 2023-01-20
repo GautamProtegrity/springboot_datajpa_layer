93556702a2e4b23a011b125d1e330ff61bb2d43a
GAUTAM <gautam2apis.access@gmail.com>
2023-01-21 00:51:51 +0530

    Pushing all implementation for initial data layer

--- src/main/java/com/bl/data/domain/dto/BookDTO.java
+++ src/main/java/com/bl/data/domain/dto/BookDTO.java
@@ -1,6 +1,7 @@
 package com.bl.data.domain.dto;
 
 import com.bl.data.domain.Author;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ public class BookDTO {
     private Integer bookCode;
     private String bookName;
     private String publisher;
+    @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
     private Author author;
     private Integer authorId;
 
